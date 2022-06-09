e69badf2dfc6732e65aa4d287ed7efdba1b65ebb
Ian Saultz <52051793+atierian@users.noreply.github.com>
2022-06-09 09:06:33 -0400

    control case - remove caching

--- Amplify/Categories/DataStore/Model/Temporal/Temporal+Cache.swift
+++ Amplify/Categories/DataStore/Model/Temporal/Temporal+Cache.swift
@@ -134,7 +134,9 @@ extension Temporal {
         in timeZone: TimeZone = .utc
     ) throws -> Foundation.Date {
         for format in formats {
-            let formatter = formatter(for: format, in: timeZone)
+            let formatter = DateFormatter() //formatter(for: format, in: timeZone)
+            formatter.dateFormat = format
+            formatter.timeZone = timeZone
             if let date = formatter.date(from: string) {
                 return date
             }
@@ -156,7 +158,9 @@ extension Temporal {
         with format: String,
         in timeZone: TimeZone = .utc
     ) -> String {
-        let formatter = formatter(for: format, in: timeZone)
+        let formatter = DateFormatter() //formatter(for: format, in: timeZone)
+        formatter.dateFormat = format
+        formatter.timeZone = timeZone
         let string = formatter.string(from: date)
         return string
     }
